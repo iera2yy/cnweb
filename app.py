8a805df32cc939b6439cc9ce296b85818a02c945
iera <346578486@qq.com>
2021-01-12 14:05:11 +0800

    自定义路由配置、静态NAT配置

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, jsonify, make_response
+from flask import Flask, render_template, jsonify, make_response, request
 from flask_cors import CORS
 import services
+import json
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+session = {"state": -1,
+           "rta": {"f0/0": "", "s0/0/0": ""},
+           "rtb": {"f0/0": "", "s0/0/0": ""},
+           "rtc": {"f0/0": "", "s0/0/0": ""}}
 
 
 # 建立连接
@@ -43,65 +48,86 @@ def format_result(result):
     return result
 
 
+def format_config(data, rt):
+    command = ['conf ter']
+    global session
+    for item in data:
+        command.append("int " + item["port"])
+        command.append("ip address " + item["ip"] + " " + item["mask"])
+        command.append("no shutdown")
+        session[rt][item["port"]] = item["ip"]
+    command.append("end")
+    return command
+
+
+def format_static(rt):
+    res = ['conf ter', 'ip subnet-zero']
+    tmp = 'ip route '
+    global session
+    if rt == 'rta':
+        tmp += '0.0.0.0 0.0.0.0 ' + session[rt]["s0/0/0"]
+    elif rt == 'rtb':
+        target_ip = session[rt]["s0/0/0"].split('.')
+        target_ip[3] = '221'
+        target_ip = '.'.join(target_ip)
+        mask = '255.255.255.224'
+        tmp += target_ip + ' ' + mask + ' ' + session[rt]["s0/0/0"]
+    elif rt == 'rtc':
+        tmp += '0.0.0.0 0.0.0.0 ' + session[rt]["f0/0"]
+    res.append(tmp)
+    return res
+
+
 # 配置路由器信息
-@app.route('/router_config')
+@app.route('/router_config', methods=["POST"])
 def config_routers():
-    clients = connect()
-    command_a = ['conf ter',
-                 'int f0/0',
-                 'ip address 10.0.0.1 255.0.0.0',
-                 'no shutdown',
-                 'int s0/0/0',
-                 'ip address 192.168.1.2 255.255.255.252',
-                 'no shutdown',
-                 'end']
-    command_b = ['conf ter',
-                 'int f0/0',
-                 'ip address 192.168.3.1 255.255.255.0',
-                 'no shutdown',
-                 'int s0/0/0',
-                 'ip address 192.168.1.1 255.255.255.252',
-                 'no shutdown',
-                 'end']
-    command_c = ['conf ter',
-                 'int f0/0',
-                 'ip address 10.0.0.2 255.0.0.0',
-                 'no shutdown',
-                 'end']
-    result = {"message": []}
-    result["message"].extend(execute_command(clients, 0, command_a))
-    result["message"].extend(execute_command(clients, 1, command_b))
-    result["message"].extend(execute_command(clients, 2, command_c))
-    disconnect(clients)
+    # clients = connect()
+    data = json.loads(request.get_data(as_text=True))
+    data_rta = data["rta"] if "rta" in data.keys() else []
+    data_rtb = data["rtb"] if "rtb" in data.keys() else []
+    data_rtc = data["rtc"] if "rtc" in data.keys() else []
+    command_a = format_config(data_rta, "rta")
+    command_b = format_config(data_rtb, "rtb")
+    command_c = format_config(data_rtc, "rtc")
+    result = {"message": [' '.join(command_a), ' '.join(command_b), ' '.join(command_c)]}
+    # result["message"].extend(execute_command(clients, 0, command_a))
+    # result["message"].extend(execute_command(clients, 1, command_b))
+    # result["message"].extend(execute_command(clients, 2, command_c))
+    # disconnect(clients)
+    global session
+    session["state"] = 0
     return make_response(jsonify(format_result(result)), 200)
+    # return make_response(jsonify(result), 200)
 
 
 # 配置静态NAT
-@app.route('/static_nat')
+@app.route('/static_nat', methods=["POST"])
 def set_static_nat():
-    clients = connect()
-    result = {"message": []}
-    # 'ip subnet-zero',
-    # RTA
-    result["message"].extend(execute_command(clients, 0, ['conf ter', 'ip subnet-zero', 'ip route 0.0.0.0 0.0.0.0 192.168.1.1']))
-    # RTB
-    result["message"].extend(execute_command(clients, 1, ['conf ter', 'ip subnet-zero', 'ip route 192.168.1.32 255.255.255.224 192.168.1.2']))
-    # RTC
-    result["message"].extend(execute_command(clients, 2, ['conf ter', 'ip subnet-zero', 'ip route 0.0.0.0 0.0.0.0 10.0.0.1']))
-    command_a = ['conf ter',
-                 'ip nat inside source static 10.0.0.2 192.168.1.34',
-                 'ip nat inside source static 10.0.0.11 192.168.1.35',
-                 'interface f0/0',
-                 'ip nat inside',
-                 'interface s0/0/0',
-                 'ip nat outside']
-    result["message"].extend(execute_command(clients, 0, command_a))
-    disconnect(clients)
-    return make_response(jsonify(format_result(result)), 200)
+    data = json.loads(request.get_data(as_text=True))
+    if "staticNat" in data.keys():
+        clients = connect()
+        data_static = data["staticNat"]
+        global session
+        result = {"message": []}
+        result["message"].extend(execute_command(clients, 0, format_static("rta")))     # RTA
+        # print(format_static("rta"))
+        result["message"].extend(execute_command(clients, 1, format_static("rtb")))     # RTB
+        # print(format_static("rtb"))
+        result["message"].extend(execute_command(clients, 2, format_static("rtc")))     # RTC
+        # print(format_static("rtc"))
+        command_a = [('ip nat inside source static ' + item["from"] + ' ' + item["to"]) for item in data_static]
+        command_a = ['conf ter'] + command_a + ['interface f0/0', 'ip nat inside', 'interface s0/0/0', 'ip nat outside']
+        result["message"].extend(execute_command(clients, 0, command_a))
+        disconnect(clients)
+        session["state"] = 1
+        # return make_response(jsonify(command_a), 200)
+        return make_response(jsonify(format_result(result)), 200)
+    else:
+        return "静态NAT配置信息不全!"
 
 
 # 删除静态NAT
-@app.route('/delete_static_nat')
+@app.route('/delete_static_nat', methods=["POST"])
 def delete_static_nat():
     clients = connect()
     command_a = ['conf ter',
@@ -111,11 +137,13 @@ def delete_static_nat():
     result = {"message": []}
     result["message"].extend(execute_command(clients, 0, command_a))
     disconnect(clients)
+    global session
+    session["state"] = 0
     return make_response(jsonify(format_result(result)), 200)
 
 
 # 配置动态NAT
-@app.route('/dynamic_nat')
+@app.route('/dynamic_nat', methods=["POST"])
 def set_dynamic_nat():
     clients = connect()
     command_a = ['conf ter',
@@ -129,11 +157,13 @@ def set_dynamic_nat():
     result = {"message": []}
     result["message"].extend(execute_command(clients, 0, command_a))
     disconnect(clients)
+    global session
+    session["state"] = 2
     return make_response(jsonify(format_result(result)), 200)
 
 
 # 显示NAT转换
-@app.route('/show_nat')
+@app.route('/show_nat', methods=["POST"])
 def show_nat():
     clients = connect()
     command_a = ['show ip nat translations',
@@ -141,12 +171,13 @@ def show_nat():
                  'show ip nat statistics']
     result = {"message": []}
     result["message"].extend(execute_command(clients, 0, command_a))
+    print(result)
     disconnect(clients)
     return make_response(jsonify(format_result(result)), 200)
 
 
 # 核验配置
-@app.route('/show_config')
+@app.route('/show_config', methods=["POST"])
 def show_command():
     clients = connect()
     command_a = ['show running-config']
